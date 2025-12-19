8fc76b4011cad86f2043827ecc6e702302070cb8
dupsys <abiodunmodupe@gmail.com>
2025-12-19 16:29:09 +0000

    Deploying to gh-pages from @ dupsys/dupsys.github.io@2ae855b716d18fa27a93113d1257f3607cfccda0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,13 +37,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "nav-dsfsi-lab",
-          title: "DSFSI Lab",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/dsfsi/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
