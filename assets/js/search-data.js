e9998b167b865a25e0744418925b5d6be88aaa5f
dupsys <abiodunmodupe@gmail.com>
2025-12-19 14:48:05 +0000

    Deploying to gh-pages from @ dupsys/dupsys.github.io@769ae448503f495cc8990db031ed87ebb8717143 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -461,14 +461,14 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/abbey_cv.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%61%62%69%6F%64%75%6E%6D%6F%64%75%70%65@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -489,7 +489,14 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=sdwzJUcAAAAJ", "_blank");
+        },
+      },{
+        id: 'social-x',
+        title: 'X',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://twitter.com/duppeabbey", "_blank");
         },
       },{
         id: 'social-custom_social',
