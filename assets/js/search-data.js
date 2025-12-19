019a59820ea418867a0a55a48846ef8257be3947
dupsys <abiodunmodupe@gmail.com>
2025-12-19 15:43:08 +0000

    Deploying to gh-pages from @ dupsys/dupsys.github.io@5e54117f643f51536d4ed54e07e81bad9b503a7c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -457,13 +457,6 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
             },},{
-        id: 'social-cv',
-        title: 'CV',
-        section: 'Socials',
-        handler: () => {
-          window.open("/assets/pdf/abbey_cv.pdf", "_blank");
-        },
-      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
@@ -471,11 +464,32 @@ ninja.data = [{
           window.open("mailto:%61%62%69%6F%64%75%6E%6D%6F%64%75%70%65@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/dupsys", "_blank");
+        },
+      },{
         id: 'social-inspire',
         title: 'Inspire HEP',
         section: 'Socials',
         handler: () => {
           window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/abiodunmodupe", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/abiodunmodupe@gmail.com", "_blank");
         },
       },{
         id: 'social-rss',
@@ -497,13 +511,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://twitter.com/duppeabbey", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
